eb3d20474c87f570380130a9998617342db2a2f7
filipo <jr_apocalipsis_69@hotmail.com>
2017-11-27 11:07:44 -0300

    commit 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,29 +7,29 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { MyApp } from './app.component';
 
 import { Home } from '../pages/home/home';
-import { MathJaxDirective } from '../directives/math-jax/math-jax';
-import { BernoulliPage } from '../pages/bernoulli/bernoulli';
-import { BinomialPage } from '../pages/binomial/binomial';
-import { ChiPage } from '../pages/chi/chi';
-import { ExponencialPage } from '../pages/exponencial/exponencial';
-import { HipergeometricaPage } from '../pages/hipergeometrica/hipergeometrica';
-import { NormalPage } from '../pages/normal/normal';
-import { PoissonPage } from '../pages/poisson/poisson';
-import { TstudentPage } from '../pages/tstudent/tstudent';
+//import { MathJaxDirective } from '../directives/math-jax/math-jax';
+//import { BernoulliPage } from '../pages/bernoulli/bernoulli';
+//import { BinomialPage } from '../pages/binomial/binomial';
+//import { ChiPage } from '../pages/chi/chi';
+//import { ExponencialPage } from '../pages/exponencial/exponencial';
+//import { HipergeometricaPage } from '../pages/hipergeometrica/hipergeometrica';
+//import { NormalPage } from '../pages/normal/normal';
+//import { PoissonPage } from '../pages/poisson/poisson';
+//import { TstudentPage } from '../pages/tstudent/tstudent';
 
 @NgModule({
   declarations: [
     MyApp,
     Home,
-    BernoulliPage,
-    BinomialPage,
-    ChiPage,
-    ExponencialPage,
-    HipergeometricaPage,
-    NormalPage,
-    PoissonPage,
-    TstudentPage,
-    MathJaxDirective
+    //BernoulliPage,
+    //BinomialPage,
+    //ChiPage,
+    //ExponencialPage,
+    //HipergeometricaPage,
+    //NormalPage,
+    //PoissonPage,
+    //TstudentPage,
+    //MathJaxDirective
   ],
   imports: [
     BrowserModule,
@@ -39,14 +39,14 @@ import { TstudentPage } from '../pages/tstudent/tstudent';
   entryComponents: [
     MyApp,
     Home,
-    BernoulliPage,
-    BinomialPage,
-    ChiPage,
-    ExponencialPage,
-    HipergeometricaPage,
-    NormalPage,
-    PoissonPage,
-    TstudentPage
+    //BernoulliPage,
+    //BinomialPage,
+    //ChiPage,
+    //ExponencialPage,
+    //HipergeometricaPage,
+    //NormalPage,
+    //PoissonPage,
+    //TstudentPage
   ],
   providers: [
     StatusBar,
